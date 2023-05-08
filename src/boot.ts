51c950bc3a29f4b69c71b0a7083ca42bd610e37e
monish karunakaran <ckmonish2000@gmail.com>
2023-05-08 19:10:32 +0530

    chore: created first route /heatbeat
    1. created a simple heartbeat route.
    2. added path alias for all folders inside src.
    3. initialised routes dir

--- src/boot.ts
+++ src/boot.ts
@@ -4,7 +4,12 @@ import {addAliases} from "module-alias"
 function setupEnvironment() {
 	dotenv.config()
 	addAliases({
-		"@utils":__dirname+"/utils"
+		"@utils":__dirname+"/utils",
+		"@services":__dirname+"/services",
+		"@models":__dirname+"/models",
+		"@router":__dirname+"/router",
+		"@schemas":__dirname+"/schemas",
+		"@controllers":__dirname+"/controllers",
 	})
 }
 
--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,13 @@
 import express,{Express} from "express"
+import router from "@router/index"
 
 function createServer():Express{
 	const app = express()
     
 	app.use(express.json())
-    
+
+	app.use(router)
+	
 	return app
 }
 
