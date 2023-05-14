ea0f100ff6ef4d0365371ed369dda3d5c8d26283
monish karunakaran <ckmonish2000@gmail.com>
2023-05-14 22:39:37 +0530

    refactor: altered auth controller
    1. replaced error handling by throwing AppError.

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,22 +2,21 @@ import { Request,Response } from "express"
 import { registerUserInputType, userLoginInputType } from "@schemas/auth.schema"
 import { createNewUser } from "@services/auth.service"
 import logger from "@utils/logger"
+import { AppError, tryCatch } from "@utils/tryCatch"
 
 
-export const registerUserHandler = async (req:Request<unknown,unknown,registerUserInputType>,res:Response)=>{
+export const registerUserHandler = tryCatch(async (req:Request<unknown,unknown,registerUserInputType>,res:Response)=>{
 	const {email,password} = req.body
 	const registered = await createNewUser(email,password)
 	
-	if(!registered.success){
-		const statusCode = registered.errorCode==="P2002" ? 409 : 500
-		const errorMessage = registered.errorCode==="P2002" ?"User with this email already exists":"Something went wrong"
-		res.status(statusCode).send(errorMessage)
+	if(!registered.success && registered.errorCode === "P2002"){
+		throw new AppError(registered.errorCode,"User with this email already exists",409)
 	}
 
 	if(registered.success){
 		res.status(201).json({id:registered?.id})
 	}
-}
+})
 
 export const userLoginHandler = (req:Request<unknown,unknown,userLoginInputType>,res:Response)=>{
 	res.sendStatus(200).json({})
