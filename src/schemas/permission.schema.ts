e27e636c43ea36a4f523235f5e077a79556343d6
monish karunakaran <ckmonish2000@gmail.com>
2023-05-21 20:24:21 +0530

    chore: created input schemas for permissions
    1. defined create permission input schema and type to accept data from req body.
    
    2. defined revoke permission input schema and type to accept data from
       params.
    
    3. defined fetch accesslist for url schema and type to accept data from
       params.

---
+++ src/schemas/permission.schema.ts
@@ -0,0 +1,25 @@
+import { TypeOf, object, string } from "zod"
+
+export const createPermissionInputSchema = object({
+	body:object({
+		userId:string({required_error:"User Id is Required"}).uuid({message:"Enter a valid UUID"}),
+		shortlink:string({required_error:"Url Id is Required"}).min(2,{message:"must be atleast 2 char long"}),
+	})
+})
+
+export const revokePermissionInputSchema = object({
+	params:object({
+		accessListId:string({required_error:"Access list id is required"})
+	})
+})
+
+export const fetchAccessListForUrlInputSchema = object({
+	params:object({
+		shortlink:string({required_error:"Url Id is Required"}).min(2,{message:"must be atleast 2 char long"}),
+	})
+})
+
+
+export type permissionInputType = TypeOf<typeof createPermissionInputSchema>["body"]
+export type revokePermissionInputType = TypeOf<typeof revokePermissionInputSchema>["params"]
+export type fetchAccessListForUrlInputType = TypeOf<typeof fetchAccessListForUrlInputSchema>["params"]
