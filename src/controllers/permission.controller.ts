ee1623e84108105aeaad3971400dadc2b15e16a2
monish karunakaran <ckmonish2000@gmail.com>
2023-05-21 20:26:32 +0530

    chore: defined the controllers fro permissions
    1. defined the addUserToAccessListHandler to provide permissions to a
       user registered on the platform.
    
    2. defined the revokeUserToAccessListHandler to revoke permissions to a
       user who already has access to a shortcut.
    
    3. defined the fetchAccessListForUrlHandler to see the list of users who
      can access the shortcut.

---
+++ src/controllers/permission.controller.ts
@@ -0,0 +1,53 @@
+import { fetchAccessListForUrlInputType, permissionInputType, revokePermissionInputType } from "@schemas/permission.schema"
+import { addUserToAccessList, fetchAccessListById, fetchAccessListForUrl, removeUserFromAccessList } from "@services/permission.service"
+import { fetchShotcutById } from "@services/shortcut.service"
+import { AppError } from "@utils/tryCatch"
+import { Request, Response } from "express"
+
+export const addUserToAccessListHandler = async(req:Request<unknown,unknown,permissionInputType>,res:Response)=>{
+	const {shortlink,userId} = req.body
+	const ownerId = req.session.user?.id
+    
+	const validateOwner = await fetchShotcutById(shortlink,ownerId)
+    
+	console.log(validateOwner,ownerId,shortlink)
+	if(!validateOwner){
+		throw new AppError("cd404","Could not provide permission as the requested shotcut was not found",404)
+	}
+
+	const memberData = await addUserToAccessList(ownerId,userId,shortlink)
+
+	res.status(201).json({
+		id:memberData.id
+	})
+}
+
+export const revokeUserToAccessListHandler =async (req:Request<revokePermissionInputType>,res:Response)=>{
+	const {accessListId} = req.params
+	const ownerId = req.session.user?.id
+    
+	const validateOwner = await fetchAccessListById(accessListId)
+
+	if(validateOwner?.shortcutUserId !== ownerId){
+		throw new AppError("cd403","You do not have permission to proceed",403)
+	}
+
+	const revokedMember = await removeUserFromAccessList(accessListId)
+
+	res.status(200).json(revokedMember)
+}
+
+export const fetchAccessListForUrlHandler =async (req:Request<fetchAccessListForUrlInputType>,res:Response)=>{
+	const {shortlink} = req.params
+	const ownerId = req.session.user?.id
+	console.log(ownerId)
+	const validateOwner = await fetchShotcutById(shortlink,ownerId)
+    
+	if(!validateOwner){
+		throw new AppError("cd404","Could not provide permission as the requested shotcut was not found",404)
+	}
+
+	const accessList = await fetchAccessListForUrl(ownerId,shortlink)
+
+	res.status(200).json(accessList)
+}
