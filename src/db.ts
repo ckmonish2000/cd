f9889713a062813f31c7e2f339e887d7be2619c4
monish karunakaran <ckmonish2000@gmail.com>
2023-05-23 17:20:45 +0530

    chore: defined redis client
    1. extraced port and host for the redis endpoint from config.
    2. created redis client called cache.

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client"
-import redis from "redis"
+import {createClient} from "redis"
+import config from "config"
+
+const redisHost = config.get<string>("redisHost")
+const redisPort = config.get<string>("redisPort")
 
 export const prisma = new PrismaClient({})
-export const cache = redis.createClient({})
+
+export const cache = createClient({
+	url:`redis://${redisHost}:${redisPort}`
+})
