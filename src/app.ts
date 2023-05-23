1b1d3d3ce9bf2f76c7034484a229967b0eb01d7e
monish karunakaran <ckmonish2000@gmail.com>
2023-05-23 17:25:11 +0530

    refactor: altered app, readme & server.d.ts
    1. deleted unused type definition file.
    2. altered readme.md file to include redis.
    3. called the connectToDB function in app.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import "./boot" // script to load dependencies
 import config from "config"
 import createServer from "@utils/server"
 import logger from "@utils/logger"
+import { connectToRedis } from "@utils/connectToDB"
+
 
 const port = config.get<number>("port")
 const app = createServer()
 
-app.listen(port,()=>{
+app.listen(port,async ()=>{
 	logger.info(`Express server running on port:${port}`)
+	await connectToRedis()
 })
