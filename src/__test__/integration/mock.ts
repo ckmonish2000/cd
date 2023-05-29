035ccdf98dc1c1566603699213ab8c48e4129743
monish karunakaran <ckmonish2000@gmail.com>
2023-05-29 16:53:32 +0530

    chore: defined mock user data

---
+++ src/__test__/integration/mock.ts
@@ -0,0 +1,9 @@
+export const primaryUser = {
+    email:"ckmonish2000@gmail.com",
+    password:"admin@123"
+}
+
+export const secondaryUser = {
+    email:"mk@gmail.com",
+    password:"admin@123"
+}
