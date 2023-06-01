6f884d9d160ad5ec6833eead6c984e93acf511fb
monish karunakaran <ckmonish2000@gmail.com>
2023-06-01 23:08:03 +0530

    chore: defined test to validate analytics data
    1. created a test to validate that the data returned is an array and the
       statuscode is 200.

---
+++ src/__test__/integration/analytics.test.ts
@@ -0,0 +1,49 @@
+import createServer from "@utils/server"
+import supertest from "supertest"
+import * as analyticsService from "@services/analytics.service"
+import * as shortcutService from "@services/shortcut.service"
+import { v4 as uuidv4 } from 'uuid';
+import { primaryUser, shortcut } from "./mock"
+
+const app = createServer()
+let cookie:string[]
+
+beforeAll(async()=>{
+        let login = await supertest(app).post("/api/login").send(primaryUser)
+        cookie = login.headers["set-cookie"]
+})
+
+describe('Analytics route', () => { 
+    
+    describe('Given valid route param', () => { 
+
+        it("should return 200",async()=>{
+            const fetchShotcutById = jest.spyOn(shortcutService,"fetchShotcutById")
+            const fetchAllLogsForShortcut = jest.spyOn(analyticsService,"fetchAllLogsForShortcut")
+
+            fetchShotcutById.mockResolvedValue({
+                userId: uuidv4(),
+                ...shortcut
+            })
+
+            fetchAllLogsForShortcut.mockResolvedValue({
+                _count:[
+                    {
+                        _count:{
+                            userId:28
+                        },
+                        userId:uuidv4()
+                    }
+                ]
+            })
+
+            const res = await supertest(app)
+            .get(`/api/analytics/${shortcut.shortlink}`)
+            .set("Cookie",cookie)
+
+            expect(Array.isArray(res.body)).toBe(true)
+            expect(res.statusCode).toBe(200)
+        })
+
+     })
+ })
