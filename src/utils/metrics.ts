3b9392b885369b0a21b630e13edf661eb6585c78
monish karunakaran <ckmonish2000@gmail.com>
2023-06-18 22:06:10 +0530

    chore: defined histograms to monitor api & db time
    1. defined responseTimeHistogram to track api response time.
    2. deined databaseResponseTimeHistogram to track db response time.

--- src/utils/metrics.ts
+++ src/utils/metrics.ts
@@ -4,9 +4,21 @@ import logger from "./logger"
 
 const app = express()
 
+export const responseTimeHistogram = new client.Histogram({
+    name:"response_time_duration_in_seconds",
+    help:"REST API response time in seconds",
+    labelNames:["method","route","status_code"]
+})
+
+export const databaseResponseTimeHistogram = new client.Histogram({
+    name:"db_response_time_duration_in_seconds",
+    help:"DB response time in seconds",
+    labelNames:["operation","success"]
+})
+
 export const startMetricServer = ()=>{
     const collectDefaultMetrics = client.collectDefaultMetrics()
-    
+
     app.get("/metrics",async(req:Request,res:Response)=>{
         res.set("Content-Type",client.register.contentType)
         return res.send(await client.register.metrics())
