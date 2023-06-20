2fd53fcec9c9e9ef37a2fa8f776b55a64934b62e
monish karunakaran <ckmonish2000@gmail.com>
2023-06-20 17:21:39 +0530

    chore: added metrics for DB response time on auth
    1. created metics timer on checkUserWithEmail and createNewUser.
    2. using metrics timer to log if the operation was successful or not
       using try catch.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,31 +1,54 @@
-import {User} from "@prisma/client"
-import {hashPassword} from "@utils/auth"
-import {prisma} from "@root/db"
+import { User } from "@prisma/client"
+import { hashPassword } from "@utils/auth"
+import { prisma } from "@root/db"
+import { databaseResponseTimeHistogram } from "@utils/metrics"
 
 export const createNewUser = async (
 	email: string,
 	password: string
 ): Promise<User> => {
-	const hashedpassword = await hashPassword(password)
+	const merticsTimer = databaseResponseTimeHistogram.startTimer()
+	const metricsLable = {
+		operation: "createNewUser"
+	}
 
-	const newUser = await prisma.user.create({
-		data: {
-			email: email,
-			password: String(hashedpassword),
-		},
-	})
+	try {
+		const hashedpassword = await hashPassword(password)
 
-	return newUser
+		const newUser = await prisma.user.create({
+			data: {
+				email: email,
+				password: String(hashedpassword),
+			},
+		})
+
+		merticsTimer({ ...metricsLable, success: "true" })
+		return newUser
+	} catch (err) {
+		merticsTimer({ ...metricsLable, success: "false" })
+		throw err
+	}
 }
 
 export const checkUserWithEmail = async (
 	email: string
 ): Promise<User | null> => {
-	const user = await prisma.user.findUnique({
-		where: {
-			email: email,
-		},
-	})
+	const merticsTimer = databaseResponseTimeHistogram.startTimer()
+	const metricsLable = {
+		operation: "checkUserWithEmail"
+	}
 
-	return user
+	try {
+		const user = await prisma.user.findUnique({
+			where: {
+				email: email,
+			},
+		})
+
+		merticsTimer({ ...metricsLable, success: "true" })
+		return user
+	} catch (err) {
+		merticsTimer({ ...metricsLable, success: "false" })
+		throw err
+	}
 }
